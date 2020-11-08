0fd02ecba256dc8c3be0f9ff8ddfb3a258111149
Ruben Rangel <ruben_rbk@hotmail.com>
2020-11-07 20:54:06 -0800

    Change Test

--- app/src/main/java/edu/cpp4310/calclkrem/ui/home/HomeViewModel.kt
+++ app/src/main/java/edu/cpp4310/calclkrem/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Hello, world!"
+        value = "Anything else"
     }
     val text: LiveData<String> = _text
 }
