68f4f7247bfa8b83a0e846236a063bfc3d3979e8
Sander <44069180+SanderAZ@users.noreply.github.com>
2020-11-07 20:58:05 -0800

    Text Change 2

--- app/src/main/java/edu/cpp4310/calclkrem/ui/home/HomeViewModel.kt
+++ app/src/main/java/edu/cpp4310/calclkrem/ui/home/HomeViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class HomeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "Anything else"
+        value = "Why did the chicken cross the road? To get to the other side!"
     }
     val text: LiveData<String> = _text
 }
